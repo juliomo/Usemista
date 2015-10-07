41b75d203942073c9ca2f74ded59e52f90518e22
Der Wunder <der.wunder.nv@gmail.com>
2015-10-07 15:45:00 -0430

    Lanzador de Aplicacion

--- app/src/main/java/com/usm/jyd/usemista/acts/ActApertura.java
+++ app/src/main/java/com/usm/jyd/usemista/acts/ActApertura.java
@@ -3,17 +3,13 @@ package com.usm.jyd.usemista.acts;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.usm.jyd.usemista.R;
 
 public class ActApertura extends AppCompatActivity {
 
-    private static final int SPLASH_TIME = 2000;
+    private static final int SPLASH_TIME = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
