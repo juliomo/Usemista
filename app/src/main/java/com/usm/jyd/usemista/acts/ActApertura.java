5aa0c90c3719e26e7ea559caeb31aa9abc9edd76
Der Wunder <der.wunder.nv@gmail.com>
2015-10-07 13:52:12 -0430

    Lanzador de Aplicacion

---
+++ app/src/main/java/com/usm/jyd/usemista/acts/ActApertura.java
@@ -0,0 +1,58 @@
+package com.usm.jyd.usemista.acts;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.usm.jyd.usemista.R;
+
+public class ActApertura extends AppCompatActivity {
+
+    private static final int SPLASH_TIME = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_apertura);
+
+
+        new AperturaDeAplicacion().execute();
+    }
+
+
+    public class AperturaDeAplicacion extends AsyncTask {
+
+        private Intent myIntent;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+             myIntent = new Intent(ActApertura.this, ActBase.class);
+        }
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+
+            try {
+                Thread.sleep(SPLASH_TIME);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            startActivity(myIntent);
+            finish();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/usm/jyd/usemista/anim/PosibleAnim.java
@@ -0,0 +1,40 @@
+package com.usm.jyd.usemista.anim;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import com.usm.jyd.usemista.acts.ActApertura;
+import com.usm.jyd.usemista.acts.ActBase;
+
+/**
+ * Created by der_w on 10/7/2015.
+ */
+//public class AperturaDeAplicacion  extends AsyncTask{
+//
+//    private  Intent myIntent
+//
+//    @Override
+//    protected void onPreExecute() {
+//        super.onPreExecute();
+//        Intent myIntent = new Intent(ActApertura.this, ActBase.class);
+//    }
+//
+//    @Override
+//    protected Object doInBackground(Object[] params) {
+//
+//        try{
+//            Thread.sleep(SPLASH_TIME);
+//        }catch (InterruptedException e){
+//            e.printStackTrace();
+//        }
+//
+//        return null;
+//    }
+//
+//    @Override
+//    protected void onPostExecute(Object o) {
+//        super.onPostExecute(o);
+//
+//        startActivity()
+//    }
+//}
